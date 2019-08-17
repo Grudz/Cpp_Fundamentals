fc44518a54bf71a3b2494cb5991c28515c09916e
Ben <50677294+bengrudzien@users.noreply.github.com>
2019-08-16 23:41:21 -0400

    Add files via upload

---
+++ Variables and Types/variables and types.cpp
@@ -0,0 +1,45 @@
+// operating with variables
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	//// declaring variables:
+	//int a, b;
+	//int result;
+
+	//// process:
+	//a = 5;
+	//b = 2;
+	//a = a + 1;
+	//result = a - b;
+
+	//// print out the result:
+	//cout << result;
+
+
+	//// terminate the program:
+	//
+
+	//int a = 1;
+	//int b(3);
+	//int c{ 7 };
+	//int result;
+
+	//a = b + c;
+	//result = a * 2;
+	//cout << result;
+	
+	string mystring;
+	
+	mystring = "You're a pussy";
+	cout << mystring << endl;
+	mystring = "No you are";
+	cout << mystring << endl;
+
+
+	return 0;
+
+}
