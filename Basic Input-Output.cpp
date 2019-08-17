0afb26d75ebdd7e04045f3bffb477b19f428e84c
Ben <50677294+bengrudzien@users.noreply.github.com>
2019-08-16 23:53:36 -0400

    Update Basic Input-Output.cpp

--- Basic Input-Output.cpp
+++ Basic Input-Output.cpp
@@ -8,21 +8,21 @@ using namespace std;
 int main()
 {
 	int i;
-	string poop;
+	string fun;
 	int feeling;
 		
-	cout << "Enter a fucking integer: ";
+	cout << "Enter a fun integer: ";
 	cin >> i;
 	cout << "You chose this value " << i << ", seriously lol?\n";
-	cout << "Lets double that shitty number " << i * 2 << "\n";
+	cout << "Lets double that number " << i * 2 << "\n";
 //	while (i<3000) //could use as password for i... interesting
-	getline(cin, poop);
+	getline(cin, fun);
 	cout << "How do you feel? >";
-	getline(cin, poop);
-	cout << "Hello " << poop << ", my name is not a pussy lol.\n";
+	getline(cin, fun);
+	cout << "Hello " << fun << ", my name is Ben lol.\n";
 	cout << "Jokes aside, how do you feel out of 10? >";
-	getline(cin, poop);
-	stringstream(poop) >> feeling;
+	getline(cin, fun);
+	stringstream(fun) >> feeling;
 	cout << "Awh you are feeling only " << feeling << " out of 10?\n";
 	
 	return 0;
