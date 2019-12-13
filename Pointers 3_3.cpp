b791e316597295111a97a377b3b4af6f8f777ced
Ben Grudzien <50677294+Grudz@users.noreply.github.com>
2019-12-13 14:46:01 -0500

    Update Pointers 3_3.cpp

--- Pointers 3_3.cpp
+++ Pointers 3_3.cpp
@@ -2,6 +2,46 @@
 #include <iostream>
 using namespace std;
 
+void increment_all (int* start, int* stop)
+{
+  int * current = start;
+  while (current != stop) {
+    ++(*current);  // increment value pointed
+    ++current;     // increment pointer so that the next pointer's value can be increased
+  }
+}
+
+void print_all (const int* start, const int* stop)
+{
+  const int * current = start;
+  while (current != stop) {
+    cout << *current << '\n';
+    ++current;     // increment pointer
+  }
+}
+
+void increase (void* data, int psize)
+{
+  if ( psize == sizeof(char) )
+  { char* pchar; pchar=(char*)data; ++(*pchar); }
+  else if (psize == sizeof(int) )
+  { int* pint; pint=(int*)data; ++(*pint); }
+}
+
+int addition (int a, int b)
+{ return (a+b); }
+
+int subtraction (int a, int b)
+{ return (a-b); }
+
+int operation (int x, int y, int (*functocall)(int,int))
+{
+  int g;
+  g = (*functocall)(x,y);
+  return (g);
+}
+
+
 int
 main ()
 {
@@ -37,20 +77,43 @@ main ()
 
 cout << "\n \n" << "~~~ Example 3 ~~~" << '\n';
 
-  int numbers[5];
+  int num[5];
   int *p; // declaring pointer p
-  p = numbers; // setting pointer = to array numbers
+  p = num; // setting pointer = to array numbers
   *p = 10;
   p++; // incrementing pointer address
   *p = 20; // one address increase = next spot in the array = 20
-  p = &numbers[2];
+  p = &num[2];
   *p = 30;
-  p = numbers + 3;
+  p = num + 3;
   *p = 40;
-  p = numbers;
+  p = num;
   *(p + 4) = 50;
   for (int n = 0; n <= 4; n++) // could also do n<5
-    cout << numbers[n] << ", ";
+    cout << num[n] << ", ";
+    
+    cout << "\n \n" << "~~~ Example 4 ~~~" << '\n';
+    
+  int numbers[] = {10,20,30};
+  increment_all (numbers,numbers+3);
+  print_all (numbers,numbers+3);
+  
+  cout << "\n \n" << "~~~ Example 5 ~~~" << '\n';
+  
+  char a = 'z'; // ASCII numbering, note: z incremented = {
+  int b = 6;
+  increase (&a,sizeof(a));
+  increase (&b,sizeof(b));
+  cout << a << ", " << b << '\n';
+  
+   cout << "\n \n" << "~~~ Example 6 ~~~" << '\n';
+  
+  int m,n;
+  int (*minus)(int,int) = subtraction;
+
+  m = operation (7, 5, addition);
+  n = operation (20, m, minus);
+  cout <<n;  
 
   return 0;
 }
