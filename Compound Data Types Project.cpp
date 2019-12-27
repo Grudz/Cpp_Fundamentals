26d3c577a5ecd68f73903323909ab89290b6768a
Ben Grudzien <50677294+Grudz@users.noreply.github.com>
2019-12-27 13:18:48 -0500

    Update Compound Data Types Project.cpp

--- Compound Data Types Project.cpp
+++ Compound Data Types Project.cpp
@@ -1,8 +1,4 @@
-// This is a work in progress
-// Ben Grudzien
-// 12/19/2019
-
- 
+// example about structures
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h> // Allows the flush funciton, clears the terminal screen
@@ -11,41 +7,127 @@
 
 using namespace std;
 
+int selection;
+
 bool a; 
 bool b;
 bool output; // Stores bits to be operated on
 bool OPERATION_OUT; // Stores operation values
 
-int selection; // Stores menu selection variable
+struct operation {
+  string all_ops;
+} AND, NAND, OR, NOR, XOR, XNOR;
 
-struct operation { // Data structure
-  string AND;
-  string DISPLAY;
-} ops;
 
-   ops.AND = "AND => a b output\n       0 0    0  \n       0 1    0  \n"
+void print_operations (operation display)
+{
+  cout << display.all_ops;
+}
+
+int main ()
+{
+    int * s_ptr; // Couldn't declare this globally
+    s_ptr = &selection;
+    
+    bool * a_ptr; 
+    a_ptr = &a;
+    
+    bool * b_ptr; 
+    b_ptr = &a;
+    
+    bool * out_ptr; 
+    out_ptr = &output;    
+    
+    bool * op_ptr; 
+    op_ptr = &OPERATION_OUT;    
+    
+    AND.all_ops = "AND => a b output\n       0 0    0  \n       0 1    0  \n"
 		     "       1 0    0  \n       1 1    1  \n--------------------\n";
 
-string DISPLAY() // Displays all the operators at once
-{
-	cout << ops.AND; 
-	return ops.AND;
+    NAND.all_ops = "NAND => a b output\n       0 0    1  \n       0 1    1  \n"
+		      "       1 0    1  \n       1 1    0  \n--------------------\n";
+		     
+    OR.all_ops = "OR => a b output\n       0 0    0  \n       0 1    1  \n"
+	        "       1 0    1  \n       1 1    1  \n--------------------\n";
+		     
+    NOR.all_ops = "NOR => a b output\n       0 0    1  \n       0 1    0  \n"
+	         "       1 0    0  \n       1 1    0  \n--------------------\n";
+		     
+    XOR.all_ops = "XOR => a b output\n       0 0    0  \n       0 1    1  \n"
+	         "       1 0    1  \n       1 1    0  \n--------------------\n";
+		     
+    XNOR.all_ops = "XNOR => a b output\n       0 0    1  \n       0 1    0  \n"
+		      "       1 0    0  \n       1 1    1  \n--------------------\n";
+		     
+	while(1)
+	{
+    cout << "Currently entered bits: " << *a_ptr << *b_ptr << "\n\n";
+	cout << "Enter the corresponding number to select option: \n"
+		" 1 : Change Bits\n 2 : Display all operators \n 3 : AND\n 4 : NAND\n"
+		" 5 : OR\n 6 : NOR\n 7 : XOR\n 8 : XNOR\n 9 : History\n 10: Exit Program\n=> ";
+	cin >> *s_ptr ;
+
+    switch (*s_ptr )
+{   
+    case 1:
+        cout << "ENTER TWO BITS (seperated by a space)\n => ";
+	    cin >> *a_ptr >> *b_ptr;
+	    cout << "--------------------\n" <<"You entered = " << *a_ptr << *b_ptr << "\n--------------------\n\n";
+
+	    *out_ptr = *a_ptr & *b_ptr; // concatenate bits together
+        break;
+    case 2:
+        print_operations (NAND);
+        print_operations (OR);
+        print_operations (NOR);
+        print_operations (XOR);
+        print_operations (XNOR);
+        print_operations (AND);
+        break;
+    case 3:
+        print_operations (AND);
+        *op_ptr = (*a_ptr & *b_ptr);
+        cout << *a_ptr << " AND " << *b_ptr << " = " << *op_ptr << "\n--------------------\n\n";
+        break;
+        
+    case 4:
+        print_operations (NAND);
+        *op_ptr = !(*a_ptr & *b_ptr);
+        cout << *a_ptr << " NAND " << *b_ptr << " = " << *op_ptr << "\n--------------------\n\n";
+        break;
+        
+    case 5:
+        print_operations (OR);
+        *op_ptr = (*a_ptr | *b_ptr);
+        cout << *a_ptr << " OR " << *b_ptr << " = " << *op_ptr << "\n--------------------\n\n";
+        break;
+        
+    case 6:
+        print_operations (NOR);
+        *op_ptr = !(*a_ptr & *b_ptr);
+        cout << *a_ptr << " NOR " << *b_ptr << " = " << *op_ptr << "\n--------------------\n\n";
+        break;
+        
+    case 7:
+        print_operations (XOR);
+        *op_ptr = (*a_ptr ^ *b_ptr);
+        cout << *a_ptr << " XOR " << *b_ptr << " = " << *op_ptr << "\n--------------------\n\n";
+        break;
+        
+    case 8:
+        print_operations (XNOR);
+        *op_ptr = !(*a_ptr ^ *b_ptr);
+        cout << *a_ptr << " XNOR " << *b_ptr << " = " << *op_ptr << "\n--------------------\n\n";
+        break;        
+        
+    case 9:
+       // History(); Store Operation_Out bits in an array
+        break;
+    case 10:
+        exit(0);
 }
 
-bool ENTER_BITS(bool output)
-{
-	cout << "ENTER TWO BITS (seperated by a space)\n => ";
-	cin >> a >> b;
-	cout << "--------------------\n" <<"You entered = " << a << b << "\n--------------------\n\n";
-
-	output = a & b; // concatenate bits together
-	return output;
+}
+  return 0;
 }
 
-
-int main()
-{
-
-    DISPLAY();
-    return 0;
-}
